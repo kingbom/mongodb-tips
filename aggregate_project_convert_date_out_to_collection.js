b25f9b2d0e9c395dc6865317c9d2cf454d23230d
kimbom <kingbom_300432@hotmail.com>
2021-08-18 23:53:51 +0700

    Update aggregate_project_convert_date_out_to_collection.js

--- aggregate_project_convert_date_out_to_collection.js
+++ aggregate_project_convert_date_out_to_collection.js
@@ -21,20 +21,28 @@ if (size == 0) {
             },
             {
                 $project: {
-                    _id: 1,
                     year: { $year: { date: "$createdDate" } },
                     month: { $month: { date: "$createdDate" } },
                     dayOfMonth: { $dayOfMonth: { date: "$createdDate" } },
-                    status: 1,
+                    email: 1,
+                    storeNo: 1,
+                    storeCode: 1,
+                    storeName: 1,
+                    merchantNo: 1,
+                    merchantName: 1,
+                    ownerFirstName: 1,
+                    ownerLastName: 1,
+                    bucket: 1,
                     resultFile: 1,
-                    errorFile: 1,
-                    message: 1,
+                    documentType: 1,
+                    documentNumber: 1,
+                    documentDate: 1,
+                    status: 1,
                     createdBy: 1,
                     createdDate: 1,
                     lastModifiedBy: 1,
                     lastModifiedDate: 1,
                     _class: 1
-
                 }
             },
             {
